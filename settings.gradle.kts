58fb1b53225e5f4871e9f962c636cd151f1ec46a
Aksyme ./ <wwznoz66f@gmail.com>
2025-10-30 12:29:47 +0200

    P1. Ktor setup

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "kotlin"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,23 @@
+package practice5
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.http.content.*
+
+fun Application.configureRouting() {
+    routing {
+        staticResources("/content", "mycontent")
+
+        get("/") {
+            call.respondText("Hello World!")
+        }
+
+        get("/test1") {
+            val text = "<h1>Hello From Ktor</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
+        }
+    }
+}
